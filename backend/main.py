85578e7f83a69986a58685cedfedf72d8f497771
ManarZrekat <93828878+ManarZrekat@users.noreply.github.com>
2022-03-05 19:55:59 +0200

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -21,10 +21,9 @@ test_transforms = transforms.Compose([transforms.Resize((224,224)),
                                       #                     [0.229, 0.224, 0.225])
                                      ])
 
-model_url = "https://firebasestorage.googleapis.com/v0/b/projectarc2021-eeed8.appspot.com/o/Potterymodel.pth?alt=media&token=cdcd6e32-d962-4618-8dd6-f80ec6000745"
+
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 model=torch.load('Potterymodel.pth')
-#model = torch.load(model_url)
 model.eval()
 
 @app.route('/predict', methods=['GET'])
@@ -33,20 +32,16 @@ def hello():
 @app.route('/predict', methods=['POST'])
 def predict_img():
         classes = ['Bowl','Jar','Juglet','Jug']
-        # imageurl = request.form.get('imageurl')
-        # print(' i got variable imageUrl with value: '+str(imageurl))
         data = {"predict_result": False}
         ######predict image#####
         file = request.files['file']
         img=Image.open(file)
-        #img_bytes = file.read()
         index = predict_image(img)
         print(classes[index])
         return classes[index]
-        #return jsonify(data) 
+        
 
-# start the flask app, allow remote connections 
-print(' ========== Load Detection Model START ========== ')
+
 
 
 def predict_image(image):
@@ -59,8 +54,7 @@ def predict_image(image):
     index = output.data.cpu().numpy().argmax()
     return index  
 
-print(' ========== Load Detection Model END ========== ')
+
 
 if __name__ == '__main__':
-    # app.run(host='0.0.0.0', port='8989')
     app.run(host='0.0.0.0', port='8989', debug=True)
